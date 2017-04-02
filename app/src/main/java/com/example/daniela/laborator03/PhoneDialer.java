05dd2d508dd14f890b5fccc081c90310d9457cfb
Daniela Dragan <daniela.dragan.14@gmail.com>
2017-04-03 00:30:45 +0300

    project created

---
+++ app/src/main/java/com/example/daniela/laborator03/PhoneDialer.java
@@ -0,0 +1,13 @@
+package com.example.daniela.laborator03;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhoneDialer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_dialer);
+    }
+}
